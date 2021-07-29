e8d5c3b56ba2d5b9cd883e0b83c33f897db77f62
SawyerOG <oglesbysaw@gmail.com>
2021-07-29 16:36:28 -0600

    query users from database

--- controllers/login_users.py
+++ controllers/login_users.py
@@ -4,68 +4,35 @@ import json
 from copy import copy
 from flask import Blueprint, request
 from app import socketio
+from app.sql import sql
+from helper_funcs.sql import dict_sql
 
 user_logging = Blueprint('user_logging', __name__)
 
-# class Users:
-
-#     number_of_users = 0
-
-#     def __init__(self, name, employeeID, authIDs):
-#         Users.number_of_users += 1
-#         self.name = name
-#         self.employeeID = employeeID
-#         self.authIDs = authIDs
-
-#         def __repr__(self):
-#             return Users.number_of_users
-
-#     def log_out_user(self):
-#         print('logging out')
-#         print(Users.number_of_users - 1)
-#         new_count = cls.number_of_users
-#         if new_count >= 0:
-#             Users.number_of_users = new_count
-#             return
-#         Users.number_of_users = 0
-#         return
-
-#     @classmethod
-#     def get_active_users(cls):
-#         return cls.number_of_users
-
-#     @classmethod
-#     def emit_active_users(cls):
-#         print('emitting', cls.number_of_users)
-#         socketio.emit('UPDATE_USER_COUNT', {'data': cls.number_of_users}, broadcast=True)
-
-
-users = {
-    'sawyer@mail.com' : {
-        'password': '1',
-        'user_name': 'Sawyer',
-        'employeeID': 'OGLES S',
-        'authIDs': [1,2,3,4,5]
-}}
+# users = {
+#     'sawyer@mail.com' : {
+#         'password': '1',
+#         'user_name': 'Sawyer',
+#         'employeeID': 'OGLES S',
+#         'authIDs': [1,2,3,4,5]
+# }}
 
 def login(data):
     """ Use the submitted credential to find and validate a user
         Expects:
+        email: string
         password: string
-        userName: string
      """
     password = data['password']
-    user_name = data['userName']
+    email = data['email']
 
-    if users.get(user_name):
-        if users[user_name]['password'] == password:
-            usr = copy(users[user_name])
-            del usr['password']
-            usr['userName'] = usr.pop('user_name')
+    c = sql.execute("SELECT [name] AS 'userName', [authIDs], [employeeID] FROM [Payroll].[dbo].[appAuth] WHERE email = ? AND password = ?", email, password)
 
-            json_user = json.dumps(usr)
-            return json_user
+    user = dict_sql(c)
 
+    if len(user) > 0:
+        print(user)
+        return user
     return False
 
 ### <-------- USER AUTH ---------> ###
